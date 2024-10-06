d28a33840d0661051322a3d620f61fd6982b838d
Markobanduka <banduka44@gmail.com>
2024-10-06 20:02:02 +0200

    Initial version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,153 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  LabelList,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  // CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+const initialChartData = [
+  { month: "OLIVERA", visitors: 0 },
+  { month: "DJUKI", visitors: 0 },
+  { month: "JULKA", visitors: 0 },
+  { month: "MILUN", visitors: 0 },
+];
+
+const chartConfig = {
+  visitors: {
+    label: "Rezultat",
+  },
+} satisfies ChartConfig;
+const Page = () => {
+  const [chartData, setChartData] = useState(initialChartData);
+  const [inputValues, setInputValues] = useState<{ [key: string]: number }>({});
+  const minValue = Math.min(...chartData.map((item) => item.visitors));
+
+  const handleInputChange = (month: string, value: string) => {
+    const numValue = parseInt(value, 10) || 0;
+    setInputValues((prev) => ({ ...prev, [month]: numValue }));
+  };
+
+  const handleSubmit = () => {
+    setChartData((prevData) =>
+      prevData.map((item) => ({
+        ...item,
+        visitors: item.visitors + (inputValues[item.month] || 0),
+      }))
+    );
+    setInputValues({});
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex justify-center items-center h-screen">
+      <Card className="w-full max-w-3xl">
+        <CardHeader>
+          <CardTitle className="flex justify-center text-2xl">
+            Remi Kalkulator
+          </CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <BarChart
+              accessibilityLayer
+              data={chartData}
+              margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+              height={300}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                axisLine={false}
+                tickLine={false}
+                tick={{
+                  fill: "hsl(210, 100%, 50%)",
+                  fontSize: 20,
+                  fontWeight: 700,
+                }}
+              />
+              <YAxis
+                domain={[0, "auto"]}
+                tickCount={4}
+                axisLine={false}
+                tickLine={false}
+                tick={{ fill: "hsl(var(--muted-foreground))" }}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel hideIndicator />}
+              />
+              <Bar dataKey="visitors" minPointSize={40}>
+                <LabelList
+                  dataKey="visitors"
+                  position="center"
+                  fill="hsl(var(--primary-foreground))"
+                  style={{ fontWeight: "bold", fontSize: "16px" }}
+                />
+                {chartData.map((item) => (
+                  <Cell
+                    key={item.month}
+                    fill={
+                      item.visitors === minValue
+                        ? "hsl(var(--destructive))"
+                        : "hsl(var(--primary))"
+                    }
+                  />
+                ))}
+              </Bar>
+            </BarChart>
+          </ChartContainer>
+          <div className="grid grid-cols-4 gap-4 mt-6">
+            {chartData.map((item) => (
+              <div key={item.month} className="flex flex-col space-y-2">
+                <Label htmlFor={`input-${item.month}`}>{item.month}</Label>
+                <Input
+                  id={`input-${item.month}`}
+                  type="number"
+                  placeholder="Unesi"
+                  value={inputValues[item.month] || ""}
+                  onChange={(e) =>
+                    handleInputChange(item.month, e.target.value)
+                  }
+                />
+              </div>
+            ))}
+          </div>
+          <Button onClick={handleSubmit} className="w-full mt-4">
+            Izracunaj
+          </Button>
+        </CardContent>
+        {/* <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="leading-none text-muted-foreground">
+          Enter values to add to each month's visitors, then click Submit.
+          Lowest value is highlighted in red.
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </CardFooter> */}
+      </Card>
     </div>
   );
-}
+};
+
+export default Page;
